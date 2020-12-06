838c043e57d8b9db270f13e00e855f5e4ed6ec64
harshitgupta09 <harshitgupta.219@gmail.com>
2020-12-07 02:46:04 +0530

    Added manual jwt encode decode

--- auth/auth_app.py
+++ auth/auth_app.py
@@ -1,31 +1,46 @@
 from flask import Flask, request
 from flask.json import jsonify
-import jwt
 import json
+import base64
+import hmac
 
 app = Flask(__name__)
 
-key = "secret"
+
 id = "student"
 password = "projectINS"
 
-@app.route("/",methods=["POST"])
-def auth():
+key = b'\xffN.\xb64\xa9\xe3\x06\xed[\xc9u\xcc\xbb&\x1f'
+def create_signed_token(data):
+    header = json.dumps({'typ': 'JWT', 'alg': 'HS256'}).encode('utf-8')
+    header_decode = base64.urlsafe_b64encode(header).decode().strip('=')
+    payload = json.dumps(data).encode('utf-8')
+    payload_decode = base64.urlsafe_b64encode(payload).decode().strip('=')
+    hp_data = header_decode + '.' + payload_decode
+    s = hmac.new(key, hp_data.encode('utf-8'), 'sha256')
+    signature = s.digest()
+    signature_decode = base64.urlsafe_b64encode(signature).decode().strip('=')
+    token = hp_data + '.' + signature_decode
+    return token
 
+
+@app.route("/",methods=["POST"])
+def auth():    
     print("AuthServer")
     credentials = json.loads(request.get_data())
 
-    if id!=credentials['username']:
+    if id!=credentials['id']:
         return jsonify({"Error": "Incorrect ID"})
     elif password!=credentials['password']:
         return jsonify({"Error": "Incorrect password"})
-        
-    encoded = jwt.encode({'key1': 'val1', 'key2': 'val2'}, key, algorithm='HS256').decode()
 
-    print("Credentials Correct. Received{}".format(credentials))
-    print("Token Generated {}".format(encoded))
+    payload = {'key1': 'val1', 'key2': 'val2'}   
+    token = create_signed_token(payload)
 
-    return jsonify({"accepted":True,"token":encoded})
+    print("Credentials Received{}".format(credentials))
+    print("Token Generated {}".format(token))
+
+    return jsonify({"accepted":True,"token":token}) 
 
 if __name__=="__main__":
     app.run(port=5000)
--- res/res_app.py
+++ res/res_app.py
@@ -2,15 +2,23 @@ from flask import Flask, request, jsonify
 import os
 import base64
 import json
-import jwt
+import hmac
 
 app = Flask(__name__)
 
 key = 'secret'
 
 def validate_token(token):
-    decoded = jwt.decode(str.encode(token), key, algorithms='HS256')
-    return True
+    (header, payload, signature) = token.split('.')
+    hp_data = header + '.' + payload
+    d = hmac.new(key, hp_data.encode('utf-8'), 'sha256')
+    dig = d.digest()
+    denc = base64.urlsafe_b64encode(dig).decode().strip('=')
+    verified = hmac.compare_digest(signature, denc)
+    payload += '=' * (-len(payload) % 4)
+    payload_data = json.loads(base64.urlsafe_b64decode(payload).decode())
+    print("Payload Data: {}".format(payload_data))
+    return verified
 
 
 @app.route("/",methods=["POST"])
